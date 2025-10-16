a4a89320edd3aab11044dcacb77d74b53f868ab3
Nur Hidayat <yayagm0712@gmail.com>
2025-10-16 22:01:30 +0800

    update tugas

--- tugas/tugas6.php
+++ tugas/tugas6.php
@@ -7,9 +7,9 @@ $deretA = [4, 6, 9, 13, 18];
 $angkaTerakhirA = end($deretA);
 $penambahan = 6; 
 $hasilA = $angkaTerakhirA + $penambahan;
-echo "Pola: Penambahan bertingkat (+2, +3, +4, +5, **+6**)<br>";
-echo "Angka selanjutnya adalah: " . $angkaTerakhirA . " + " . $penambahan . " = **" . $hasilA . "**<br>";
-echo "Deret lengkap: " . implode(", ", $deretA) . ", **" . $hasilA . "**";
+echo "Pola: Penambahan bertingkat (+2, +3, +4, +5, +6)<br>";
+echo "Angka selanjutnya adalah: " . $angkaTerakhirA . " + " . $penambahan . " = " . $hasilA . "<br>";
+echo "Deret lengkap: " . implode(", ", $deretA) . ", " . $hasilA . "";
 
 echo "<hr>";
 
@@ -17,9 +17,9 @@ echo "<h3>b. 2, 2, 3, 3, 4, ??</h3>";
 $deretB = [2, 2, 3, 3, 4];
 $angkaTerakhirB = end($deretB);
 $hasilB = $angkaTerakhirB;
-echo "Pola: Setiap angka diulang dua kali (2, 2, 3, 3, **4, 4**)<br>";
-echo "Angka selanjutnya adalah: **" . $hasilB . "**<br>";
-echo "Deret lengkap: " . implode(", ", $deretB) . ", **" . $hasilB . "**";
+echo "Pola: Setiap angka diulang dua kali (2, 2, 3, 3, 4, 4)<br>";
+echo "Angka selanjutnya adalah: " . $hasilB . "<br>";
+echo "Deret lengkap: " . implode(", ", $deretB) . ", " . $hasilB . "";
 
 echo "<hr>";
 
@@ -29,9 +29,9 @@ $angkaGenapSebelumnya = $deretC[3];
 $hasilC = $angkaGenapSebelumnya + 1;
 echo "Pola: Dua sub-deret berjalan bergantian:<br>";
 echo "  - Posisi Ganjil: 1, 2, 3, ... (+1)<br>";
-echo "  - Posisi Genap: 9, 10, **11**, ... (+1)<br>";
-echo "Angka selanjutnya adalah: " . $angkaGenapSebelumnya . " + 1 = **" . $hasilC . "**<br>";
-echo "Deret lengkap: " . implode(", ", $deretC) . ", **" . $hasilC . "**";
+echo "  - Posisi Genap: 9, 10, 11, ... (+1)<br>";
+echo "Angka selanjutnya adalah: " . $angkaGenapSebelumnya . " + 1 = " . $hasilC . "<br>";
+echo "Deret lengkap: " . implode(", ", $deretC) . ", " . $hasilC . "";
 
 echo "<hr>";
 
